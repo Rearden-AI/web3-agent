16d09ddad81bd3ec2744f71bfcbd0e93075faba7
serjou1 <sergnik97@gmail.com>
2024-07-08 09:11:10 +0000

    added env lo log path

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -5,8 +5,16 @@ from datetime import datetime
 
 
 def configure_logging():
+    env = os.getenv('ENV')
+
     if not os.path.exists('log'):
         os.mkdir('log')
+
+    if not os.path.exists(f'log/{env}'):
+        os.mkdir(f'log/{env}')
+
+    log_time = '{:%Y-%m-%d_%H:%M:%S}'.format(datetime.now())
+    log_path = f'log/{env}/{log_time}.log'
 
     logging.basicConfig(format='%(asctime)s [%(name)s] [%(levelname)s]: %(message)s',
                         datefmt='%H:%M:%S',
@@ -14,7 +22,7 @@ def configure_logging():
                         handlers=[
                             logging.StreamHandler(stream=sys.stdout),
                             logging.FileHandler(
-                                filename='log/{:%Y-%m-%d_%H:%M:%S}.log'.format(datetime.now())
+                                filename=log_path
                             )
                         ])
     logging.getLogger('watchfiles.main').setLevel(logging.WARNING)

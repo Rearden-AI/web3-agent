6ba01cf4e57fe844d88591b019f826de1b72cf9b
serjou1 <sergnik97@gmail.com>
2024-07-16 16:24:11 +0000

    fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,21 @@
 # pylint: disable=wrong-import-position
 import logging
+import traceback
+
 
 from contextlib import asynccontextmanager
 from fastapi.responses import JSONResponse, PlainTextResponse
 import redis.asyncio as redis
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException, Request, status, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
 from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
 from apscheduler.schedulers.background import BackgroundScheduler
+from fastapi.routing import APIRoute
+from typing import Callable, List, Union
+
+
 
 from app.logging_config import configure_logging
 configure_logging()
@@ -37,7 +43,6 @@ async def lifespan(app: FastAPI):
     scheduler.start()
     redis_connection = redis.from_url(redis_config.REDIS_URL, encoding="utf8")
 
-    # await llm_driver.initialize()
 
     print("Started lifespan")
     await FastAPILimiter.init(redis_connection)
@@ -59,49 +64,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 app.include_router(router=router_v1, prefix=config.api_v1_prefix)
 
 @app.exception_handler(HTTPException)
 async def exception_handler(request: Request, exc: HTTPException):
+    logger.info("Handling http exeption")
     msg = (await format_request(request)) + "\n\n" + str(exc)
 
     await telegram.notify_error(msg)
     return await http_exception_handler(request, exc)
 
 
-async def set_body(request: Request, body: bytes):
-    async def receive():
-        return {"type": "http.request", "body": body}
+@app.exception_handler(Exception)
+async def exception_handler_2(request: Request, exc: Exception):
+    msg = f"URL: {request.url}\n\n"
 
-    request._receive = receive
+    # print(request)
 
+    msg += traceback.format_exc()
 
-async def get_body(request: Request) -> bytes:
-    body = await request.body()
-    await set_body(request, body)
-    return body
-
-
-@app.middleware("http")
-async def errors_handling(request: Request, call_next):
-    body = await request.body()
-    try:
-        await set_body(request, body)
-        return await call_next(request)
-    except Exception as exc:
-        msg = f"URL: {request.url}\n"
-        msg += f"Body: {body.decode()}"
-        msg += "\n\n"
-        msg += str(exc)
-
-        await telegram.notify_error(msg)
-
-        return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            content=str(exc)
-        )
-
+    await telegram.notify_error(msg)
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content="Server error!"
+    )
 
 
 async def format_request(request: Request):
--- app/telegram/__init__.py
+++ app/telegram/__init__.py
@@ -7,7 +7,11 @@ tg_config = TelegramConfig()
 bot = telegram.Bot(token=tg_config.TG_TOKEN)
 
 async def notify(msg: str, chat_id=tg_config.TG_CHAT_ID):
-    await bot.send_message(chat_id=chat_id, text=msg)
+    if len(msg) > 4096:
+        for x in range(0, len(msg), 4096):
+            await bot.send_message(chat_id, msg[x:x+4096])
+    else:
+        await bot.send_message(chat_id, msg)
 
 async def notify_error(msg):
     await notify(msg=msg, chat_id=tg_config.TG_ERROR_CHAT_ID)

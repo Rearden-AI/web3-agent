5d2b530cfc506f7651b5ea0b0b0494dad9cccfb7
nothern05 <gleb5shimko@gmail.com>
2024-07-23 13:23:02 +0300

    upds dockerfile

--- vs_main.py
+++ vs_main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from vectorstore_updater_app import app
 
 if __name__ == '__main__':
     uvicorn.run("vs_main:vectorstore_updater_app", port=6017, host="0.0.0.0", reload=True, reload_excludes=["*.log"])
